fb043ea4220cd5ab793ce4af986ced4107d30814
Brayan Albaro Garcia Taboada <garcia.brayan3001@gmail.com>
2025-10-23 20:20:40 -0400

    prueba

--- apps/productos/serializers.py
+++ apps/productos/serializers.py
@@ -2,10 +2,6 @@ from rest_framework import serializers
 from .models import Producto
 
 class ProductoSerializer(serializers.ModelSerializer):
-    # Campos de solo lectura para mostrar las etiquetas legibles
-    genero_display = serializers.CharField(source='get_genero_display', read_only=True)
-    temporada_display = serializers.CharField(source='get_temporada_display', read_only=True)
-    
     class Meta:
         model = Producto
         fields = '__all__'
